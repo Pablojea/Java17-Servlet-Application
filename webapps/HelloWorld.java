b0ab80afc95835705a51d90bc5847472949c3c52
Pablojea <pablo_ojea@outlook.com>
2023-04-29 21:47:08 +0200

    first servlet working

--- webapps/HelloWorld.java
+++ webapps/HelloWorld.java
@@ -1,5 +1,27 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.io.*;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+
+public class HelloWorld extends HttpServlet {
+    public void doGet(HttpServletRequest req, HttpServletResponse res){
+
+        try {
+            this.log("se imprime en tomcat/logs/localhost.fechadehoy.log");
+            System.out.println("se imprime en tomcat/logs/catalina.out");
+            res.setContentType("text/html");
+            PrintWriter out = res.getWriter();
+            String user = req.getParameter("user");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Hello World!</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hello " + user + " World!</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+        catch (Exception ex) {
+            System.out.println("Algo fue mal: " + ex.toString());
+        }
     }
 }
