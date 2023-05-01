c86573b55e8c57fe37dd9442468431256c42e7ce
Pablojea <pablo_ojea@outlook.com>
2023-05-01 17:02:21 +0200

    añade funcionalidad de getQ2Songs en DataModel.java

--- webapps/p2/DataModel.java
+++ webapps/p2/DataModel.java
@@ -4,9 +4,12 @@ import java.util.ArrayList;
 
 public abstract class DataModel {
 
+
+    // ahora mismo se crean manualmente varios idiomas para poder mostrar algo en la primera pantalla
     static ArrayList<String> getQ2Langs(){
 
         ArrayList<String> langs = new ArrayList<>();
+
         langs.add("es");
         langs.add("en");
         langs.add("fr");
@@ -18,9 +21,50 @@ public abstract class DataModel {
 
     }
 
-    // ArrayList<Song> getQ2Song(String lang){
+    // en este momento see crean algunas canciones falsas para algunos de los idiomas creados
+    // de momento sólo contienen título, idioma, y sid, para poder continuar a la última pantalla
+    static ArrayList<Song> getQ2Songs(String lang){
+        
+        ArrayList<Song> allSongs = new ArrayList<>();
+        String auxLang = "";        
 
-    // }
+        for(int i = 0; i < 9; i++){
+
+            if(i % 2 ==0){
+                auxLang = "es";
+
+            }else{
+                auxLang = "it";
+            }
+            
+            allSongs.add(new Song("song" + i, auxLang, "1"));
+        }
+
+        for(int i = 0; i < 9; i++){
+            if(i % 2 ==0){
+                auxLang = "de";
+
+            }else{
+                auxLang = "gl";
+            }
+
+            allSongs.add(new Song("song" + i, auxLang, "2"));
+        }
+
+        ArrayList<Song> langSongs = new ArrayList<>();
+
+        for(Song song: allSongs){
+
+            if(song.getLang().equals(lang)){
+                langSongs.add(song);
+            }
+            
+        }
+
+        return langSongs;
+    }
+
+
 
     // ArrayList<Album> getQ2Albums(String lang, String sid){
 
